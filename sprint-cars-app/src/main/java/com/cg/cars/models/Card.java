e394c1c390460b1b2168387569557d2a22440473
ABHISHEK <mail2abhisheksen@gmail.com>
2021-04-09 15:15:22 +0530

    commit

--- sprint-cars-app/src/main/java/com/cg/cars/models/Card.java
+++ sprint-cars-app/src/main/java/com/cg/cars/models/Card.java
@@ -2,9 +2,86 @@ package com.cg.cars.models;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Card {
-	private String cardName;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column
+	private String name;
+	@Column
 	private String cardNumber;
-    private LocalDate cardExpiry;
+	@Column
+    private LocalDate expiry;
+	@Column
     private int cvv;
+    
+	public Card() {
+	}
+
+	public Card(long id, String name, String number, LocalDate expiry, int cvv) {
+		this.id = id;
+		this.name = name;
+		this.cardNumber = number;
+		this.expiry = expiry;
+		this.cvv = cvv;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNumber() {
+		return cardNumber;
+	}
+
+	public void setNumber(String number) {
+		this.cardNumber = number;
+	}
+
+	public LocalDate getExpiry() {
+		return expiry;
+	}
+
+	public void setExpiry(LocalDate expiry) {
+		this.expiry = expiry;
+	}
+
+	public int getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(int cvv) {
+		this.cvv = cvv;
+	}
+
+	@Override
+	public String toString() {
+		return "Card [id=" + id + ", name=" + name + ", cardNumber=" + cardNumber + ", expiry=" + expiry + ", cvv=" + cvv + "]";
+	}
+	
+	
+    
+    
+    
 }
--- sprint-cars-app/src/main/java/com/cg/cars/repositories/IPaymentRepository.java
+++ sprint-cars-app/src/main/java/com/cg/cars/repositories/IPaymentRepository.java
@@ -2,12 +2,14 @@ package com.cg.cars.repositories;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
 import com.cg.cars.models.Payment;
 
-public interface IPaymentRepository {
-	public Payment addPayment(Payment payment);
-	public Payment removePayment(long id);
-	public Payment updatePayment(long id, Payment payment);
-	public Payment getPaymentDetails(long id);
-	public List<Payment> getAllPaymentDetails();
+public interface IPaymentRepository extends CrudRepository<Payment, Long>{
+//	public Payment addPayment(Payment payment);
+//	public Payment removePayment(long id);
+//	public Payment updatePayment(long id, Payment payment);
+//	public Payment getPaymentDetails(long id);
+//	public List<Payment> getAllPaymentDetails();
 }
