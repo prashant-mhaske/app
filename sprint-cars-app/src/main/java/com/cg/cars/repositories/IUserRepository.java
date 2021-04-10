94c6a1e77a90c56d641719d752dc0ccdd50ac596
abhisen <abhisen@LIN51012033.corp.capgemini.com>
2021-04-10 15:49:24 +0530

    Commit

--- sprint-cars-app/src/main/java/com/cg/cars/repositories/IUserRepository.java
+++ sprint-cars-app/src/main/java/com/cg/cars/repositories/IUserRepository.java
@@ -1,10 +1,12 @@
 package com.cg.cars.repositories;
 
+import org.springframework.data.repository.CrudRepository;
+
 import com.cg.cars.models.User;
 
-public interface IUserRepository {
-	public User signIn(User user);
-	public User signOut(User user);
-	public User changePassword(long id, User user);
+public interface IUserRepository extends CrudRepository<User, Long>{
+//	public User signIn(User user);
+//	public User signOut(User user);
+//	public User changePassword(long id, User user);
 
 }
--- sprint-cars-app/src/main/java/com/cg/cars/services/CustomerService.java
+++ sprint-cars-app/src/main/java/com/cg/cars/services/CustomerService.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.cars.models.Customer;
 import com.cg.cars.models.Payment;
 import com.cg.cars.repositories.IAddressRepository;
 import com.cg.cars.repositories.ICustomerRepository;
 
-public class CustomerServiceImpl implements ICustomerService 
+@Service
+public class CustomerService implements ICustomerService 
 {
 	@Autowired
 	ICustomerRepository customerRepository;
@@ -21,7 +23,8 @@ public class CustomerServiceImpl implements ICustomerService
 	@Override
 	public Customer addCustomer(Customer customer) {
 		
-		return null;
+		customerRepository.save(customer);
+		return customer;
 	}
 
 	@Override
