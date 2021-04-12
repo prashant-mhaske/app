1af70011308a23f9a768bcf9d585a7f1ab150f9e
abhisen <abhisen@LIN51012033.corp.capgemini.com>
2021-04-12 10:09:10 +0530

    User, Customer and admin

--- sprint-cars-app/src/main/java/com/cg/cars/controllers/UserController.java
+++ sprint-cars-app/src/main/java/com/cg/cars/controllers/UserController.java
@@ -1,5 +1,43 @@
 package com.cg.cars.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.cars.models.User;
+import com.cg.cars.services.UserService;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	UserService userService;
+	
+	@PostMapping("/SignIn/{userId}/{password}")
+	public User signIn(@PathVariable ("userId") long userId, @PathVariable ("password") String password)
+	{
+		return userService.signIn(userId, password);
+		
+	}
+	
+	@GetMapping("/SignOut")
+	public User signOut(@RequestBody User user)
+	{
+		
+		
+		return userService.signOut(user);
+	}
+	
+	
+	@PutMapping("/ChangePassword/{id}")
+	public User changePassword(@PathVariable ("id") long id, @RequestBody User user) 
+	{
+		return userService.changePassword(id, user);
+	}
 }
--- sprint-cars-app/src/main/java/com/cg/cars/models/Admin.java
+++ sprint-cars-app/src/main/java/com/cg/cars/models/Admin.java
@@ -1,5 +1,42 @@
 package com.cg.cars.models;
 
-public class Admin {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Admin extends User {
+	
+	@Id
+	private long userId;
+
+	public Admin() {
+		super();
+		
+	}
+
+	public Admin(long userId) {
+		super();
+		super.setRole("admin");
+		this.userId = userId;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [userId=" + userId + "]";
+	}
+
+	
+	
+	
 
 }
