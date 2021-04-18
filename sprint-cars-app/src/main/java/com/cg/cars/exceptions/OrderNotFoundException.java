6a69f4dae12ce297108a16eb6ef8b8ef3b4d0914
abhisen <abhisen@LIN51012033.corp.capgemini.com>
2021-04-18 12:34:12 +0530

    commit

---
+++ sprint-cars-app/src/main/java/com/cg/cars/exceptions/OrderNotFoundException.java
@@ -0,0 +1,19 @@
+package com.cg.cars.exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	Logger log = LoggerFactory.getLogger(OrderNotFoundException.class);
+
+	public OrderNotFoundException(String msg) {
+		super(msg);
+    log.info("Order not Found !!");
+	}
+
+
+		
+	}
+	
